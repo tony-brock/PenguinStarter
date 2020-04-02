ba19d9542d16ef4cfebad372113a3f18346df017
tony-brock <60753793+tony-brock@users.noreply.github.com>
2020-04-01 22:07:40 -0400

    Update penguin.js

--- penguin.js
+++ penguin.js
@@ -1,1 +1,11 @@
-<function proimse
+var classPromise = d3.json("classData.json");
+
+classPromise.then(function(student)
+                  {
+                    console.log(student);
+                  }
+                  function(error)
+                  {
+                    console.log("error",error);
+                  });
+
